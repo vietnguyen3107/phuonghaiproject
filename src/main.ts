31c61a3a292257dae57f0571b25e5bde9f8dd37e
thanhnew2001 <69960815+thanhnew2001@users.noreply.github.com>
2021-11-15 11:58:38 +0700

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+// somewhere in your initialization file
 
 
 declare const module: any;
@@ -7,7 +9,8 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-
+  app.use(helmet());
+  
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
